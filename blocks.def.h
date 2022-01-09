a89875feb47a76a503bde97664583f09c7c36e37
Nathaniel Evan <nathanielevan@zohomail.com>
2022-01-09 13:04:30 +0700

    Removes icons from dwmblocks
    Icons are now printed by the scripts themselves

--- blocks.def.h
+++ blocks.def.h
@@ -3,10 +3,10 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{""   , "dwmblocks-net", 2, 0},
 	{""   , "dwmblocks-vol", 2, 10},
-	{"  ", "dwmblocks-ram", 2, 0},
-	{"﬙  ", "dwmblocks-cpu", 2, 0},
+	{""   , "dwmblocks-ram", 2, 0},
+	{""   , "dwmblocks-cpu", 2, 0},
 	{""   , "dwmblocks-bat", 2, 0},
-	{"  ", "dwmblocks-time", 5, 0},
+	{""   , "dwmblocks-time", 5, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
