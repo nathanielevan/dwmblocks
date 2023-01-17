5d44bf00b31471b4320e615e2f4d591d7978ecd9
Nathaniel Evan <nathanielevan@zohomail.com>
2023-01-18 04:33:24 +0700

    Add bluetooth status and consolidate ethernet and wlan status into status-net

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{""   , "status-eth", 2, 0},
-	{""   , "status-wlan", 2, 0},
 	{""   , "status-vol", 2, 10},
 	{""   , "status-bat", 2, 0},
+	{""   , "status-bth", 2, 0},
+	{""   , "status-net", 2, 0},
 	{""   , "status-mon", 2, 0},
 	{""   , "status-time", 5, 0},
 };
