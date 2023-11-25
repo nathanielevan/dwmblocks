0dfdd6aefa4781a0a5fa35c40e7ab2196e582ea6
Nathaniel Evan <nathanielevan@zohomail.com>
2023-11-25 13:06:35 +0700

    Add --nficons toggle for scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{""   , "status-vol", 2, 10},
-	{""   , "status-bat", 2, 0},
-	{""   , "status-bth", 2, 0},
-	{""   , "status-net", 2, 0},
-	{""   , "status-mon", 2, 0},
-	{""   , "status-time", 5, 0},
+	{""   , "status-vol --nficons", 2, 10},
+	{""   , "status-bat --nficons", 2, 0},
+	{""   , "status-bth --nficons", 2, 0},
+	{""   , "status-net --nficons", 2, 0},
+	{""   , "status-sys --nficons", 2, 0},
+	{""   , "status-time --nficons", 5, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
